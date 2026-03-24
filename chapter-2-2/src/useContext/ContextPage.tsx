dbb026001691a1b1137f66139d9c80f38bb4a71a
hunbee776 <hunbee776@gmail.com>
2026-03-24 11:25:04 +0900

    Refactor ContextPage component structure
    Removed redundant import statement and adjusted class names.

--- chapter-2-2/src/useContext/ContextPage.tsx
+++ chapter-2-2/src/useContext/ContextPage.tsx
@@ -1,13 +1,12 @@
 import { ThemeProvider } from "./context/ThemeProvider";
 import Navbar from "./Navbar";
 import ThemeContent from "./ThemeContent";
-//import { ThemeProvider } from "./context/ThemeProvider";
 
 export default function ContextPage() {
 
   return (
     <ThemeProvider>
-    <div className='flex flex-col items-center justify-center min-h-screen'>    
+    <div className='flex flex-col justify-center min-h-screen'>    
     <Navbar />
 
     <main className='flex-1'>
