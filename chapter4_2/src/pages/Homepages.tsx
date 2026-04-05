dee46ff5a91481c3e84e7e8147b422327d5bfb25
hunbee776 <hunbee776@gmail.com>
2026-04-05 18:45:13 +0900

    mission 2

--- chapter4_2/src/pages/Homepages.tsx
+++ chapter4_2/src/pages/Homepages.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 const Homepage = () => {
     return (
         <div className="h-dvh flex flex-col">
-        <nav>dddd</nav>
+        <nav>내비게이션</nav>
         <main className="flex-1">
         <Outlet />
         </main>
