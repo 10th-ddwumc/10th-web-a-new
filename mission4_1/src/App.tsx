b684cbd465c296ca07da7bf548759191e28b260b
최유연 <164215451+yuyeon48@users.noreply.github.com>
2026-04-06 11:33:22 +0900

    주석처리

--- mission4_1/src/App.tsx
+++ mission4_1/src/App.tsx
@@ -7,7 +7,7 @@ import Layout from './components/Layout'; // Layout만 가져오면 됩니다.
 function App() {
   return (
     <Routes>
-      {/* 부모 루트를 Layout으로 설정하여 모든 페이지에 공통 UI 적용 */}
+
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} /> 
         <Route path="/movies" element={<MoviesPage />} /> 
