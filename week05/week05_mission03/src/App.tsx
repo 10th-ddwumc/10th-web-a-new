abaea74c3a5fe4a8a5cab4a89c252836cc3d6127
ChaeYeon-J <jjso0467@gmail.com>
2026-04-29 00:09:41 +0900

    FEAT: Social Login Google

---
+++ week05/week05_mission03/src/App.tsx
@@ -0,0 +1,35 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import LoginPage from "./pages/LoginPage";
+import HomeLayout from "./layouts/HomeLayout";
+import SignupPage from "./pages/SignupPage";
+import MyPage from "./pages/MyPage";
+import { AuthProvider } from "./context/AuthContext";
+import GoogleLoginRedirectPage from "./pages/GoogleLoginRedirectPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "login", element: <LoginPage /> },
+      { path: "signup", element: <SignupPage /> },
+      { path: "my", element: <MyPage /> },
+      { path: "v1/auth/google/callback", element: <GoogleLoginRedirectPage /> },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  );
+}
+
+export default App;
